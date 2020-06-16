a8f0686b1582cfec846479a2306e32ab058a5d8a
Shaimaa.Mamdouh <Shaimaa.Mamdouh@EG-SHIAMAAM.STA.com>
2020-06-16 19:03:10 +0200

    First commit for Clean code POC

---
+++ MercuryToursProject/src/test/java/MercuryToursPackage/FindFlight.java
@@ -0,0 +1,32 @@
+package MercuryToursPackage;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+public class FindFlight {
+
+	@Test
+	public void ClickOnFlightsLinks() {
+		LaunchBrowser.driver.findElement(By.linkText("Flights")).click();
+		//selecting "one way" option
+		LaunchBrowser.driver.findElement(By.xpath("//body//b//input[2]")).click();
+		//Selecting August from "On" drop down list
+		LaunchBrowser.driver.findElement(By.xpath("//select[@name='fromMonth']//option[contains(text(),'August')]")).click();
+		LaunchBrowser.driver.findElement(By.name("findFlights")).click();
+		String Noflights= LaunchBrowser.driver.findElement(By.xpath("/html[1]/body[1]/div[2]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[1]/td[1]/p[1]/font[1]/b[1]/font[1]")).getText();
+
+		/*if(Noflights.contains("No Seats Avaialble")) {
+
+			test.pass("No flights found with the selected search critera");
+		}
+		else
+		{
+			test.log(Status.INFO, "This step shows usage of log(status, details)");
+		}
+		extent.flush();*/
+	}
+
+}
